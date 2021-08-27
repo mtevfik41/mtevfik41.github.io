a14ad7bae186e8ca4f6962a09927b3407920a702
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-27 05:29:06 +0300

    Project finished there will be just data update

--- src/components/ProjectsInHomeSection/ProjectsCardHome.js
+++ src/components/ProjectsInHomeSection/ProjectsCardHome.js
@@ -5,7 +5,7 @@ const ProjectsCardHome = ({header, description, url}) => {
   return (
       <div className="projects-in-home-card">
         <h1 className="project-header">{header}</h1>
-        <p className="project-description">{description}</p>
+        <p className="project-description">{description.substr(0, 115)}...</p>
         <a href={url} target="_blank" rel="noreferrer" className="project-link">View
           on &nbsp;<FaGithub/></a>
       </div>
